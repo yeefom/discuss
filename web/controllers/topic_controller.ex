d7f0362bcaa946896ee93dc01edc69f1885d61c9
Yifeng Wang <yifeng@yeefom.com>
2018-09-28 21:08:36 -0700

    handle creation error

--- web/controllers/topic_controller.ex
+++ web/controllers/topic_controller.ex
@@ -9,6 +9,13 @@ defmodule Discuss.TopicController do
   end
 
   def create(conn, %{ "topic" => topic }) do
+    changeset = Topic.changeset(%Topic{}, topic)
 
+    case Repo.insert(changeset) do
+      { :ok, post } -> IO.inspect(post)
+      { :error, changeset } ->
+        IO.inspect changeset
+        render conn, "new.html", changeset: changeset
+    end
   end
 end
