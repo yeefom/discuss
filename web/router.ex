74b860709150a7d8298ef65eb1b7f1db6c231114
Yifeng Wang <yifeng@yeefom.com>
2018-09-29 17:30:52 -0700

    add edit

--- web/router.ex
+++ web/router.ex
@@ -19,10 +19,7 @@ defmodule Discuss.Router do
     get "/", TopicController, :index
     get "/topics/new", TopicController, :new
     post "/topics", TopicController, :create
+    get "/topics/:id/edit", TopicController, :edit
+    put "/topics/:id", TopicController, :update
   end
-
-  # Other scopes may use custom stacks.
-  # scope "/api", Discuss do
-  #   pipe_through :api
-  # end
 end
