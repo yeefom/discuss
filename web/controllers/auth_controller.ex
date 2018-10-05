9bd1098bac723e50f6d0538d34f7873658ccbf52
Yifeng Wang <yifeng@yeefom.com>
2018-10-04 21:05:05 -0700

    upsert user

--- web/controllers/auth_controller.ex
+++ web/controllers/auth_controller.ex
@@ -2,7 +2,21 @@ defmodule Discuss.AuthController do
   use Discuss.Web, :controller
   plug Ueberauth
 
-  def callback(conn, params) do
+  alias Discuss.User
 
+  def callback(%{assigns: %{ueberauth_auth: auth}}, %{provider: provider}) do
+    user_params = %{token: auth.credentials.params, email: auth.info.email, provider: provider}
+    changeset = User.changeset(%User{}, user_params)
+
+    upsert_user(changeset)
+  end
+
+  defp upsert_user(changeset) do
+    case Repo.get_by(User, email: changeset.changes.email) do
+      nil ->
+        Repo.insert(changeset)
+      user ->
+        {:ok, user}
+    end
   end
 end
